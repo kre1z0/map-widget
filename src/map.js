b86bf0931352885acc42a3ef1dafc5a949ffc378
kreizo <thekreizo@gmail.com>
2018-03-06 13:56:37 +0200

    points

--- src/map.js
+++ src/map.js
@@ -1,11 +1,11 @@
 import fetchJsonp from 'fetch-jsonp';
 
-import { init } from '../../sGis/dist/init.js';
-import { TileLayer } from '../../sGis/dist/layers/TileLayer.js';
-import { FeatureLayer } from '../../sGis/dist/layers/FeatureLayer.js';
-import { PointFeature } from '../../sGis/dist/features/PointFeature.js';
-import { BalloonControl } from '../../sGis/dist/controls/BalloonControl.js';
-import { Point } from '../../sGis/dist/Point.js';
+import { init } from 'sGis/dist/init.js';
+import { TileLayer } from 'sGis/dist/layers/TileLayer.js';
+import { FeatureLayer } from 'sGis/dist/layers/FeatureLayer.js';
+import { PointFeature } from 'sGis/dist/features/PointFeature.js';
+import { BalloonControl } from 'sGis/dist/controls/BalloonControl.js';
+import { Point } from 'sGis/dist/Point.js';
 
 const apiUrl = 'https://msp.everpoint.ru/';
 
@@ -16,45 +16,50 @@ class Map {
 
     fetchData() {
         return fetchJsonp(`${apiUrl}static/fair.jsonp`, {
-            jsonpCallback: 'cb'
+            jsonpCallbackFunction: 'callback',
         })
-        .then(function(response) {
-            return response.json()
-        }).then(function(json) {
-            console.log('parsed json', json)
-        }).catch(function(ex) {
-            console.log('parsing failed', ex)
-        })
+            .then(res => res.json())
+            .then(json => json)
+            .catch(ex => console.log('Error', ex))
     }
 
     init() {
-        this.fetchData();
-        let points = [
-            {
-                position: [55.7514, 37.6409],
-                text: 'Moscow',
-                link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/MSK_Collage_2015.png/343px-MSK_Collage_2015.png'
-            },
-            {
-                position: [59.9226, 30.3324],
-                text: 'Saint Petersburg',
-                link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/St._Petersburg_Montage_2016.png/343px-St._Petersburg_Montage_2016.png'
-            }
-        ];
-        let { map, painter } = init({
-            wrapper: document.body,
-            layers: [new TileLayer('http://b.tile.openstreetmap.org/{z}/{x}/{y}.png')],
-            centerPoint: new Point([57.84, 40.56]),
-            resolution: 2445.984905125002
+        this.fetchData().then(data => {
+            console.log('--> data',data);
+            console.log('--> features', data.features.features);
+            let points = [
+                {
+                    position: [55.7514, 37.6409],
+                    text: 'Moscow',
+                    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/MSK_Collage_2015.png/343px-MSK_Collage_2015.png'
+                },
+                {
+                    position: [59.9226, 30.3324],
+                    text: 'Saint Petersburg',
+                    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/St._Petersburg_Montage_2016.png/343px-St._Petersburg_Montage_2016.png'
+                }
+            ];
+
+            let { map, painter } = init({
+                wrapper: document.body,
+                layers: [new TileLayer('http://b.tile.openstreetmap.org/{z}/{x}/{y}.png')],
+                centerPoint: new Point([57.84, 40.56]),
+                resolution: 2445.984905125002
+            });
+
+            let control = new BalloonControl(map, { painter });
+            let featureLayer = new FeatureLayer();
+
+            data.features.features.forEach(({ geometry, properties }) => {
+                console.log('--> feature.geometry', geometry);
+                console.log('--> feature.properties.address', properties.address);
+                let feature = new PointFeature(geometry);
+                control.attach(feature, `<h1>${properties.address}</h1>`);
+                featureLayer.add(feature);
+            });
+
+            map.addLayer(featureLayer);
         });
-        let control = new BalloonControl(map, { painter });
-        let featureLayer = new FeatureLayer();
-        points.forEach(point => {
-            let feature = new PointFeature(point.position);
-            control.attach(feature, `<h1>${point.text}</h1><img src="${point.link}" />`);
-            featureLayer.add(feature);
-        });
-        map.addLayer(featureLayer);
     }
 }
 
